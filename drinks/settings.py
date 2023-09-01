fe0ad55f2b826f47589bc1090358ccbab678ccd9
dav-sh <dav.llwyr@gmail.com>
2023-08-31 18:19:09 -0600

    GET METHOD example  working properly

--- drinks/settings.py
+++ drinks/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'drinks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drinks',
+    
 ]
 
 MIDDLEWARE = [
